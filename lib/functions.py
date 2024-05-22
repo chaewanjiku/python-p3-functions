08e42004bbab4c940e3aaa46c5ef55fd23c893a1
Charity <charitywanjiku8245@gmail.com>
2024-05-22 18:38:25 +0300

    Chae is done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
-def greet(name):
-    pass
+def greet(name = "Chae"):
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
-
+    return num1 + num2
+    result = 16 + 8
+    print(result)
 def halve(number):
-    pass
+    return number/2
+    result = 100/2
+    print(result)
